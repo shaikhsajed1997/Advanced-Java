ace7bf8f2cd7202082a85faa3edd8b4417758e0e
shaikhsajed1997 <99971031+shaikhsajed1997@users.noreply.github.com>
2023-07-28 17:13:10 +0530

    Crud operation with Hibernate

---
+++ Hibernate CRUD operation java base/src/com/tech/main/Execute.java
@@ -0,0 +1,35 @@
+package com.tech.main;
+
+import java.util.Scanner;
+
+public class Execute {
+
+	public static void main(String[] args) {
+
+		Operation op = new Operation();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Select choice:\n1.savaData\n2.getAlldata\n3.updateData\n4.deleteData");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			op.saveData();
+			break;
+		case 2:
+			op.getAllData();
+			break;
+		case 3:
+			op.updateData();
+			break;
+		case 4:
+			op.daleteData();
+			break;
+
+		default:
+			System.out.println("Please select correct choice");
+			break;
+		}
+
+	}
+
+}
---
+++ Hibernate CRUD operation java base/src/com/tech/util/HibernateUtil.java
@@ -0,0 +1,45 @@
+package com.tech.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.MySQL55Dialect;
+
+import com.tech.entity.Employee;
+
+public class HibernateUtil {
+	
+	private static StandardServiceRegistry rgs;
+	private static SessionFactory sf;
+	public static SessionFactory getSessionFact()
+	{
+		
+		Map<String, Object> set=new HashMap<>();
+		set.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+		set.put(Environment.URL, "jdbc:mysql://localhost:3306/hbemployeedb");
+		set.put(Environment.USER, "root");
+		set.put(Environment.PASS, "root");
+		
+		set.put(Environment.HBM2DDL_AUTO, "update");
+		set.put(Environment.SHOW_SQL, "true");
+		set.put(Environment.DIALECT, "org.hibernate.dialect.MySQL55Dialect");
+		
+		rgs=new StandardServiceRegistryBuilder().applySettings(set).build();
+		
+		MetadataSources msd=new MetadataSources(rgs).addAnnotatedClass(Employee.class);
+		
+		Metadata md=msd.getMetadataBuilder().build();
+		
+		sf=md.getSessionFactoryBuilder().build();
+		
+		return sf;
+		
+	}
+
+}
