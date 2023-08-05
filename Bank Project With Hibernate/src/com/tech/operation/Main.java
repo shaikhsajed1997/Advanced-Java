4109785ff7de95b76f9234ffcae91db2c8dc89da
shaikhsajed1997 <99971031+shaikhsajed1997@users.noreply.github.com>
2023-08-05 15:39:57 +0530

    Bank project with Hibernate

---
+++ Bank Project With Hibernate/src/com/tech/operation/Main.java
@@ -0,0 +1,44 @@
+package com.tech.operation;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Execute ex= new Execute();
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+		System.out.println("Select Operation:\n1.createAccount\n2.showAccount"
+				+ "\n3.depositMoney\n4.balanceCheck\n5.withdrawMoney\n6.TranferMoney");
+		int choice=sc.nextInt();
+		switch (choice) {
+		case 1:
+			ex.createAccount();
+			break;
+		case 2:
+			ex.showAccountDetails();
+			break;
+		case 3:
+			ex.depositMoney();
+			break;
+			
+		case 4:
+			ex.balanceCheck();
+			break;
+		case 5:
+			ex.withdrawMoney();
+			break;
+		case 6:
+			ex.transferMoney();
+			break;
+			
+		default:
+			System.out.println("Please select correct choice");
+			break;
+		}
+
+	}
+	}
+
+}
---
+++ Bank Project With Hibernate/src/com/tech/util/HibernateUtil.java
@@ -0,0 +1,43 @@
+package com.tech.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+import com.tech.entity.Account;
+
+
+public class HibernateUtil {
+
+	private static StandardServiceRegistry rgs;
+	private static SessionFactory sf;
+	public static SessionFactory getSessionFactory()
+	{
+		Map<String, Object> set= new HashMap<>();
+		set.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+		set.put(Environment.URL, "jdbc:mysql://localhost:3306/bankdb");
+		set.put(Environment.USER, "root");
+		set.put(Environment.PASS, "root");
+		
+		set.put(Environment.HBM2DDL_AUTO, "update");
+		//set.put(Environment.SHOW_SQL, "true");
+		set.put(Environment.DIALECT, "org.hibernate.dialect.MySQL55Dialect");
+		
+		rgs=new StandardServiceRegistryBuilder().applySettings(set).build();
+		
+		MetadataSources msd= new MetadataSources(rgs).addAnnotatedClass(Account.class);
+		
+		Metadata md= msd.getMetadataBuilder().build();
+		
+		sf=md.getSessionFactoryBuilder().build();
+		
+		return sf;
+		
+	}
+}
